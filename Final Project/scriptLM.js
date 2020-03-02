c87edcce032ed08ad1c63f2119792edbf8ddad11
Liam Mackenzie <liam.mackenzie@FVHYK429J1WK.local>
2020-03-02 13:42:28 -0500

    got github working again march 2

---
+++ Final Project/scriptLM.js
@@ -0,0 +1,14 @@
+console.log("working")
+
+
+var countries = ["Canada","Ethiopia","Kenya","Somolia"]
+var year = [2015, 2015, 2015, 2015]
+var mmratio = [7,353,510,732]
+
+document.getElementById("clickMe")
+
+
+
+
+
+
