52a56bf184e47de8c654e6057d2675f59743af0f
Liam Mackenzie <liam.mackenzie@FVHYK429J1WK.local>
2020-03-30 13:40:08 -0400

    worked on some stylistic elements, script3.js is still giving me difficulties

--- Final Project/script3.js
+++ Final Project/script3.js
@@ -1,4 +1,3 @@
-flag=true;
 console.log("script3")
 
 
@@ -6,13 +5,14 @@ console.log("script3")
     var year = [2015, 2015, 2015, 2015]
     var mmratio = [7,353,510,732]
 
-    document.getElementById("goal3button")
 
     document.getElementById("goal3button").onclick = clicked;
 
     
 
     function clicked() {
+        
+        
         var value = document.getElementById("fname").value;
         
         
@@ -20,35 +20,13 @@ console.log("script3")
         for (i = 0; i < countries.length; i = i + 1) {
 
             if (value === countries[i]) {
-                console.log(value + " has a population of" + mmratio[i]);
-                document.getElementById("results").innerHTML = value+" has a maternal mortality ratio of "+mmratio[i] + " per 100,000 live births" + " in the year " + year[i] + ".";
-            }
-               
-            if (value !== countries[i]);
-                flag = false;
-                document.getElementById("results").innerHTML = "There is no data for this country at the moment. Countries with data are: Canada, Ethiopia, Kenya, and Somolia";
-            
-                flag = true;
-            
-           
-            
-            
-     /*   else {
-             document.getElementById("results").innerHTML = "Data for this country is not available yet. Countries with data are : Canada, Ethiopia, Kenya, and Somalia. Please try inputting one of these countries to view data. Inputs are case sensitive." ;
+                document.getElementById("results").innerHTML = value+" had a maternal mortality ratio of "+mmratio[i] + " deaths per 100,000 live births" + " in the year " + year[i] + ".";
                 
-            
+            } else if (value !== countries[i]){
+                    document.getElementById("results").innerHTML = "There is no data for this country at the moment. Countries with data are: Canada, Ethiopia, Kenya, and Somolia";
             }
-            */
-
-            }
-
-        }
-
-
-
+           }
+           
+    }
+        
     
-
-
-
-
-
