163e55e34b36976f12ddf08d5de0a8086fdb186b
Liam Mackenzie <liam.mackenzie@FVHYK429J1WK.local>
2020-04-02 12:06:48 -0400

    work apr 2 2020

---
+++ Final Project/script8.js
@@ -0,0 +1,43 @@
+
+var countries = ["CANADA","ETHIOPIA","KENYA","SOMOLIA"]
+var year = [2015, 2015, 2015, 2015]
+var mmratio = [7,353,510,732]
+
+
+document.getElementById("goal8button").onclick = clicked;
+
+function clicked() {
+	console.log("clicked")
+    
+    loc = -1;
+    
+    value = document.getElementById("fname").value.toUpperCase();
+    console.log(value);
+    
+    for (i = 0; i < countries.length; i = i + 1) {
+        
+        if (countries[i] === value) {
+            
+            ptag = document.getElementById("results")
+            
+            
+            ptag.innerHTML = value+" had a maternal mortality ratio of "+mmratio[i] + " deaths per 100,000 live births" + " in the year " + year[i] + ".";
+            
+            console.log("found it")
+            return;
+        }   
+        
+    }
+    
+    
+   
+   
+        ptag = document.getElementById("results")
+    
+        ptag.innerHTML = document.getElementById("results").innerHTML = "There is no data for this country at the moment. Countries with data are: Canada, Ethiopia, Kenya, and Somolia";
+    
+        console.log("not there")
+    
+
+}
+    
